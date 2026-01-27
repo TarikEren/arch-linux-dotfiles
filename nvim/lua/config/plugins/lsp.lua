03acc13ce2f9a76907c51af3be23ca515f9ce03e
TarikEren <tarikerentosun@outlook.com>
2026-01-27 15:56:00 +0000

    Updated old version check command

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -34,34 +34,8 @@ return {
 
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
-          -- -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          -- ---@param client vim.lsp.Client
-          -- ---@param method vim.lsp.protocol.Method
-          -- ---@param bufnr? integer some lsp support methods only in specific files
-          -- ---@return boolean
-          -- local function client_supports_method(client, method, bufnr)
-          --   if vim.fn.has 'nvim-0.11' == 1 then
-          --     return client:supports_method(method, bufnr)
-          --   else
-          --     return client.supports_method(method, { bufnr = bufnr })
-          --   end
-          -- end
-
-          -- NOTE: This might be deprecated in the future, check what calls this function and fix them if deprecated.
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+          if client and client:supports_method("textDocument/documentHighlight", event.buf) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
